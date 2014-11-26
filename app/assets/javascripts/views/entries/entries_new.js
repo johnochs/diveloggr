30bfd04097486569465d146b82afb40479aa6bdf
John Ochs <johnochs@me.com>
2014-11-26 13:13:15 -0800

    Adding new entry form and view for creating a new entry

--- app/assets/javascripts/views/entries/entries_new.js
+++ app/assets/javascripts/views/entries/entries_new.js
@@ -1,8 +1,15 @@
 Diveloggr.Views.EntriesNew = Backbone.CompositeView.extend({
 	template: JST['entries/new_form'],
+	events: {
+		"submit": "submitForm"
+	},
 	render: function () {
 		var renderedContent = this.template({ model: this.model });
 		this.$el.html(renderedContent);
 		return this;
+	},
+	submitForm: function (event) {
+		this.$form = $("#entry_input")
+		
 	}
 });

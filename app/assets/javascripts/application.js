466e5cef9aa1247eeba768c6043108cff8d7be92
John Ochs <johnochs@me.com>
2014-11-26 16:22:36 -0800

    Updating application to require serializejson library

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require underscore
 //= require backbone
 //= require diveloggr
+//= require jquery.serializejson.min
 //= require_tree ./utils
 //= require_tree ../templates
 //= require_tree ./models

f297b2d9bc8feeb3d064662982c0c4c080695b10
John Ochs <johnochs@me.com>
2014-11-26 16:21:51 -0800

    Modifying the posts controller to update the user_id for the incoming post with the current_user's id

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -7,6 +7,7 @@ class Api::PostsController < ApplicationController
   
   def create
     @post = Post.new(post_params)
+    @post.user_id = current_user.id
     
     if @post.save
       render json: @post
@@ -18,6 +19,14 @@ class Api::PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     render json: @post
+  end
+  
+  private
+  
+  def post_params
+    params.require(:entry).permit(
+      :title, :body, :airtemp, :divenum, :location_name, :logdate, :maxdepth, :vis, :watertemp
+    )
   end
   
 end

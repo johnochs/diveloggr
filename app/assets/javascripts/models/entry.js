d04858686ec59068fd56697591468eb26ec37042
John Ochs <johnochs@me.com>
2014-11-29 23:10:06 -0800

    Fixed entry to parse user into a proper model so that user attributes can be obtained through the #user() method

--- app/assets/javascripts/models/entry.js
+++ app/assets/javascripts/models/entry.js
@@ -2,9 +2,15 @@ Diveloggr.Models.Entry = Backbone.Model.extend({
 	urlRoot: "api/entries",
 	user: function () {
 		if (!this._user) {
-			this._user = Diveloggr.Collections.users.getOrFetch(this.attributes.user_id);
+			this._user = new Diveloggr.Models.User({ id: this.attributes.user_id });
 		}
-		// debugger
 		return this._user;
+	},
+	parse: function (jsonResp) {
+		if (jsonResp.user) {
+			this.user().set(jsonResp.user, { parse: true });
+			delete jsonResp.user;
+		}
+		return jsonResp;
 	}
 });

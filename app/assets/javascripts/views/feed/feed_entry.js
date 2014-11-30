29453ce7ff70b5de4eb5e9860b2780783aaa192f
John Ochs <johnochs@me.com>
2014-11-30 02:37:07 -0800

    Starting to work on some event actions for the feed entries

--- app/assets/javascripts/views/feed/feed_entry.js
+++ app/assets/javascripts/views/feed/feed_entry.js
@@ -4,10 +4,18 @@ Diveloggr.Views.FeedEntry = Backbone.CompositeView.extend({
 		this.model.once("sync", this.render, this);	
 	},
 	template: JST['feed/feed_entry'],
+	className: "feed-entry-item",
 	tagName: "tr",
+	events: {
+		"mouseover td": "specialThing"
+	},
 	render: function() {
 		var renderedContent = this.template({ entry: this.model });
 		this.$el.html(renderedContent);
 		return this;
 	},
+	specialThing: function(event) {
+
+		alert("ding!" + this.model.id);
+	}
 })

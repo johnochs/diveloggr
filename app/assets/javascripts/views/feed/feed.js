72ec17e0791352fa3b0fa8aa83d902b264be4473
John Ochs <johnochs@me.com>
2014-11-29 17:37:31 -0800

    Continual futzing with getting the map to load.

--- app/assets/javascripts/views/feed/feed.js
+++ app/assets/javascripts/views/feed/feed.js
@@ -2,13 +2,14 @@ Diveloggr.Views.FeedView = Backbone.CompositeView.extend({
 	className: "container-fluid",
 	template: JST['feed/feed'],
 	initialize: function () {
-		this.listenTo(this.collection, "sync", this.render)
+		this.listenTo(this.collection, "sync", this.render);
 		this.listenTo(this.collection, "add", this.addFeedEntryView);
+		this.listenTo(this.collection, "remove", this.removeFeedEntryView);
 	},
 	render: function () {
-		// this.remove();
 		this.$el.html(this.template());
 		this.attachSubviews();
+		debugger
 		this.renderMap();
 		return this;
 	},
@@ -26,6 +27,14 @@ Diveloggr.Views.FeedView = Backbone.CompositeView.extend({
 	addFeedEntryView: function (entry) {
 		var entrySubview = new Diveloggr.Views.FeedEntry({ model: entry });
 		this.addSubview("#entry-table-elements", entrySubview);
+	},
+	removeFeedEntryView: function (entry) {
+		var entrySubview = _.find(
+			this.subviews("#entry-table-elements"), function(subview) {
+				return subview.model === entry;
+			}
+		);
+		this.removeSubview("#entry-table-elements", entrySubview);
 	},
 
 });

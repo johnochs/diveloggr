1c250f62ffabc4fad96923c26190d54b7592aeb9
John Ochs <johnochs@me.com>
2014-11-29 20:53:06 -0800

    Adding users controller to serve JSON user data

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,16 @@
+class Api::UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+    render json: @users
+  end
+  
+  
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+  
+  private
+  
+end

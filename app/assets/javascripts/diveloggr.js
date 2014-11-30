8752f9611bd451ef1c82b4a25819f26cecffbf5a
John Ochs <johnochs@me.com>
2014-11-29 18:18:59 -0800

    changes to the way that goog maps is loaded within the app. Entries are now loading and the map is showing.

--- app/assets/javascripts/diveloggr.js
+++ app/assets/javascripts/diveloggr.js
@@ -1,5 +1,13 @@
+var mapEl = $('<div id="map-viewport"></div>');
+var mapOptions = {
+      center: {lat: 37.7533, lng: -122.452},
+      zoom: 12
+    };
+
 
 window.Diveloggr = {
+  $mapEl: mapEl,
+  map: new google.maps.Map(mapEl[0], mapOptions),
   Models: {},
   Collections: {},
   Views: {},
--- app/assets/javascripts/views/feed/feed.js
+++ app/assets/javascripts/views/feed/feed.js
@@ -5,24 +5,19 @@ Diveloggr.Views.FeedView = Backbone.CompositeView.extend({
 		this.listenTo(this.collection, "sync", this.render);
 		this.listenTo(this.collection, "add", this.addFeedEntryView);
 		this.listenTo(this.collection, "remove", this.removeFeedEntryView);
+		this.collection.once("sync", this.renderMap, this);
 	},
 	render: function () {
 		this.$el.html(this.template());
 		this.attachSubviews();
-		debugger
+		this.$('#map-container').html(Diveloggr.$mapEl);
 		this.renderMap();
 		return this;
 	},
 	renderMap: function() {
-		function initialize() {
-		  var mapOptions = {
-		    zoom: 9,
-		    center: new google.maps.LatLng(33.674497, -119.223633)
-		  };
-		  map = new google.maps.Map(document.getElementById('map-viewport'),
-		      mapOptions);
-		}
-		google.maps.event.addDomListener(window, 'load', initialize);
+	    google.maps.event.trigger(Diveloggr.map, 'resize');
+	    Diveloggr.map.setCenter(mapOptions.center);
+	    Diveloggr.map.setZoom(9);
 	},
 	addFeedEntryView: function (entry) {
 		var entrySubview = new Diveloggr.Views.FeedEntry({ model: entry });

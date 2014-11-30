4d4c9ad155487d538a5e84e70dcefdadf8f13ac2
John Ochs <johnochs@me.com>
2014-11-30 11:28:06 -0800

    Adding click handlers to navigate to specific entry show page

--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -1,3 +1,3 @@
 Diveloggr.Models.User = Backbone.Model.extend({
-	urlRoot: "api/users"
+	urlRoot: "api/users",
 });
--- app/assets/javascripts/views/feed/feed_entry.js
+++ app/assets/javascripts/views/feed/feed_entry.js
@@ -7,15 +7,25 @@ Diveloggr.Views.FeedEntry = Backbone.CompositeView.extend({
 	className: "feed-entry-item",
 	tagName: "tr",
 	events: {
-		"mouseover td": "specialThing"
+		"mouseenter": "highliteItem",
+		"mouseleave": "backToNormal",
+		"click": "goShow"
 	},
 	render: function() {
 		var renderedContent = this.template({ entry: this.model });
 		this.$el.html(renderedContent);
 		return this;
 	},
-	specialThing: function(event) {
-
-		alert("ding!" + this.model.id);
+	highliteItem: function(event) {
+		event.currentTarget.style.background = "black";
+		event.currentTarget.style.color = 'white';
+	},
+	backToNormal: function(event) {
+		event.currentTarget.style.background = "";
+		event.currentTarget.style.color = "";
+	},
+	goShow: function(event) {
+		var entryid = $(event.currentTarget).find(".ei-indicator").data('entry-id');
+		Backbone.history.navigate("#entries/" + entryid, { trigger: true });
 	}
 })

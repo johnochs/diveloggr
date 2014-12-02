38e35c055cb7945e816760d8a276c74d6825c52b
John Ochs <johnochs@me.com>
2014-12-02 13:18:20 -0800

    Added an array to collect GM event listeners

--- app/assets/javascripts/views/entries/entries_form.js
+++ app/assets/javascripts/views/entries/entries_form.js
@@ -1,13 +1,18 @@
 Diveloggr.Views.EntriesForm = Backbone.CompositeView.extend({
 	initialize: function () {
+		this.googEL = [];
 		this.listenTo(this.model, "sync", this.render);
-		this.listenTo(this.model, "sync", this.updateSelected);
 		this.listenTo(this.model, "sync", this.updateMap);
 		this.updateMap();
-		google.maps.event.addListenerOnce(
+		this.googEL.push(
+			google.maps.event.addListenerOnce(
 			Diveloggr.map, 'click', this.addDragMarker.bind(this)
+			)
 		);
-		google.maps.event.addListener(Diveloggr.map, 'idle', this.render.bind(this));
+		debugger
+		this.googEL.push(
+			google.maps.event.addListener(Diveloggr.map, 'idle', this.render.bind(this))
+		)
 	},
 	template: JST['entries/new_form'],
 	className: "new_entry_form container",
@@ -15,23 +20,19 @@ Diveloggr.Views.EntriesForm = Backbone.CompositeView.extend({
 		"submit": "submitForm"
 	},
 	render: function () {
-		google.maps.event.trigger( Diveloggr.map, 'resize');
 		var renderedContent = this.template({ entry: this.model });
 		this.$el.html(renderedContent);
 		this.$('#map-container').html(Diveloggr.$mapEl);
 		google.maps.event.trigger(Diveloggr.map, 'resize');
+		this.updateSelected();
+		this.parseTime();
 		return this;
 	},
 	submitForm: function (event) {
 		event.preventDefault();
 		var formInput = $('#entry_form_el').serializeJSON();
 		var entry = this.model.set(formInput);
-		
-		function success () {
-			Backbone.history.navigate("/feed", { trigger: true});
-		}
-		
-		
+				
 		if (entry.isNew()) {
 			this.collection.create(entry, {
 				success: success
@@ -41,6 +42,13 @@ Diveloggr.Views.EntriesForm = Backbone.CompositeView.extend({
 				success: success
 			});
 		}
+		function success () {
+			Backbone.history.navigate("/feed", { trigger: true});
+		}
+		
+		_(this.googEL).each( function (eventlistener) {
+			google.maps.event.removeListener(eventlistener);
+		});
 	},
 	updateSelected: function () {
 		var that = this;
@@ -55,14 +63,16 @@ Diveloggr.Views.EntriesForm = Backbone.CompositeView.extend({
 	},
 	updateMap: function () {
 		if (!this.model.isNew()) {
-			var lL = new google.maps.LatLng(
-						parseFloat(this.model.get('latitude')),
-						parseFloat(this.model.get('longitude'))
-					 );
-			Diveloggr.map.panTo(lL);
-			Diveloggr.map.setZoom(15);
+			if(this.model.get('latitude')) {
+				var lL = new google.maps.LatLng(
+							parseFloat(this.model.get('latitude')),
+							parseFloat(this.model.get('longitude'))
+						 );
+				Diveloggr.map.panTo(lL);
+				Diveloggr.map.setZoom(15);
+			}
 		} else {
-			var lL = new google.maps.LatLng(37.781083, -122.411542)
+			var lL = new google.maps.LatLng(37.781083, -122.411542);
 			Diveloggr.map.panTo(lL);
 			Diveloggr.map.setZoom(15);
 		}
@@ -78,7 +88,9 @@ Diveloggr.Views.EntriesForm = Backbone.CompositeView.extend({
 		
 		this.marker = newMarker;
 		this.updateLatLng(newMarker.getPosition());
-		google.maps.event.addListener(this.marker, 'position_changed', this.readLatLng.bind(this));
+		this.googEL.push(
+			google.maps.event.addListener(this.marker, 'position_changed', this.readLatLng.bind(this))
+		);
 	},
 	readLatLng: function () {
 		var lL = this.marker.getPosition();
@@ -93,7 +105,12 @@ Diveloggr.Views.EntriesForm = Backbone.CompositeView.extend({
 		
 		this.$lat.attr('value', latLng.lat());
 		this.$lng.attr('value', latLng.lng());
+	},
+	parseTime: function () {
+		if (this.model.escape('entrytime')) {
+			var timeStr = this.model.escape('entrytime');
+			var parsedTime = timeStr.match(/:\d+:\d+/)[0].slice(1);
+			this.$el.find('#entrytime').attr('value', parsedTime);
+		}
 	}
-	
-	
 });

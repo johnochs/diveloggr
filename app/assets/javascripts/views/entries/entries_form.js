fd8c8797a723e84b0dbebc45746ef1720210938f
John Ochs <johnochs@me.com>
2014-12-01 16:34:51 -0800

    Adding div for map container

--- app/assets/javascripts/views/entries/entries_form.js
+++ app/assets/javascripts/views/entries/entries_form.js
@@ -1,7 +1,9 @@
 Diveloggr.Views.EntriesForm = Backbone.CompositeView.extend({
 	initialize: function () {
+		this.model.fetch();
 		this.listenTo(this.model, "sync", this.render);
 		this.listenTo(this.model, "sync", this.updateSelected);
+		this.listenTo(this.model, "sync", this.updateMap);
 	},
 	template: JST['entries/new_form'],
 	className: "new_entry_form container",
@@ -11,6 +13,8 @@ Diveloggr.Views.EntriesForm = Backbone.CompositeView.extend({
 	render: function () {
 		var renderedContent = this.template({ entry: this.model });
 		this.$el.html(renderedContent);
+		this.$('#map-container').html(Diveloggr.$mapEl);
+		google.maps.event.trigger(Diveloggr.map, 'resize');
 		return this;
 	},
 	submitForm: function (event) {
@@ -37,12 +41,18 @@ Diveloggr.Views.EntriesForm = Backbone.CompositeView.extend({
 		var that = this;
 		_(this.model.attributes).each( function (value, attribute) {
 			var searchId = "* #" + attribute;
-			console.log(searchId);
 			that.$el.find(searchId).each( function (){
 				if($(this).val() === value) {
 					$(this).prop("checked", true);
 				}
 			});
 		});
+	},
+	updateMap: function () {
+		debugger
+		if (!this.model.isNew()) {
+			// Diveloggr.map.panTo(latLng: { lat: this.model.get('latitude'), lng: this.model.get('longitude')});
+			Diveloggr.map.setZoom({ zoom: 15 });
+		}
 	}
 });

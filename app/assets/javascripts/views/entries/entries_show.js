b41c0785a797bf46b7334d923385b296d96c1fd2
John Ochs <johnochs@me.com>
2014-12-04 00:40:18 -0800

    Adding a listener to the images collection

--- app/assets/javascripts/views/entries/entries_show.js
+++ app/assets/javascripts/views/entries/entries_show.js
@@ -1,6 +1,7 @@
 Diveloggr.Views.EntriesShow = Backbone.CompositeView.extend({
 	initialize: function () {
 		this.listenTo(this.model, "sync", this.render)
+		this.listenTo(this.model.images(), "add remove sync", this.render)
 		var wrapper = google.maps.event.addListenerOnce(Diveloggr.map, 'tilesloaded', this.panMap.bind(this));
 		this.addGoogEL(wrapper);
 	},

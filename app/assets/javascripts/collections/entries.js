4916317ed5e8bd4cfd2a502f5f50ff0bc57ad451
John Ochs <johnochs@me.com>
2014-12-04 14:46:16 -0800

    Removing old code.

--- app/assets/javascripts/collections/entries.js
+++ app/assets/javascripts/collections/entries.js
@@ -38,18 +38,5 @@ Diveloggr.Collections.entries = new Diveloggr.Collections.Entries;
 //Immediately fetches the collection and builds the Diveloggr markerHash
 
 Diveloggr.Collections.entries.mfetch();
-// 	success: function() {
-// 		Diveloggr.Collections.entries.each( function(entry) {
-// 			var marker = new google.maps.Marker({
-// 				position: new google.maps.LatLng(
-// 					parseFloat(entry.get('latitude')), parseFloat(entry.get('longitude'))
-// 				),
-// 				map: Diveloggr.map,
-// 				title: entry.escape('title'),
-// 				icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
-// 			});
-// 			Diveloggr.markerHash[entry.get('id')] = marker;
-// 		});
-// 	}
-// });
+
 

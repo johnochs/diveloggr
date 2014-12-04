29056d82fc8bf6ee78a1bb6df1743bd5d6b700a6
John Ochs <johnochs@me.com>
2014-12-04 14:45:21 -0800

    Removing old code

--- app/assets/javascripts/models/entry.js
+++ app/assets/javascripts/models/entry.js
@@ -17,20 +17,8 @@ Diveloggr.Models.Entry = Backbone.Model.extend({
 			this.user().set(jsonResp.user, { parse: true });
 			delete jsonResp.user;
 		}
-		// debugger
 		var that = this;
 		if (jsonResp.images) {
-			// jsonResp.images.forEach( function (image) {
-			// 	var model = new Diveloggr.Models.Image;
-			// 	var trueModel = model.set(image);
-			// 	debugger
-			// 	if (trueModel.get('imageable_id') === parseInt(that.get('id'), 10)) {
-			// 		if (model.get('l_url')) {
-			// 			that.images().add(model);
-			// 		}
-			// 		// debugger
-			// 	}
-			// })
 			this.images().reset(jsonResp.images, { parse: true });
 		}
 		return jsonResp;

f989180b32f520e55d408ea4cae8df7010ae47eb
John Ochs <johnochs@me.com>
2014-12-04 15:42:26 -0800

    moving forward with user show view

--- app/assets/javascripts/views/users/show.js
+++ app/assets/javascripts/views/users/show.js
@@ -1,5 +1,6 @@
 Diveloggr.Views.UserShow = Backbone.CompositeView.extend({
 	template: JST['users/show'],
+	className: "container",
 	initialize: function () {
 		this.listenTo(this.model, "sync", this.render);
 	},

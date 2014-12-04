dd57143258507c0cc155942926f0c8c3fbda4771
John Ochs <johnochs@me.com>
2014-12-03 23:49:01 -0800

    Adding index route for api/images

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:update, :show, :index, :update]
     resources :entries, only: [:create, :update, :index, :show, :destroy]
-    resources :images, only: [:create, :show, :destroy, :update]
+    resources :images, only: [:create, :show, :destroy, :update, :index]
   end
   
 end

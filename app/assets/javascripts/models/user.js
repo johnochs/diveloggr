0f3df1d5523160503248f0637c99378e117b8988
John Ochs <johnochs@me.com>
2014-12-05 04:51:21 -0800

    Massive overhaul the the ridiculous #parse I previously had

--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -4,9 +4,23 @@ Diveloggr.Models.User = Backbone.Model.extend({
 		if(!this._entries) {
 			this._entries = new Diveloggr.Collections.Entries;
 		}
-		//return this._entries.where({ user_id: parseInt(this.id) })
-		var filteredArray = this._entries.where({ user_id: parseInt(this.id) });
-		this._entries = new Diveloggr.Collections.Entries(filteredArray, { parse: true });
 		return this._entries;
 	},
+	image: function () {
+		if(!this._image) {
+			this._image = new Diveloggr.Models.Image;
+		}
+		return this._image;
+	},
+	parse: function (jsonResp) {
+		if(jsonResp.entries) {
+			this.entries().set(jsonResp.entries, { parse: true });
+			delete jsonResp.entries;
+		}
+		if(jsonResp.image) {
+			this.image().set(jsonResp.image, { parse: true});
+			delete jsonResp.image;
+		}
+		return jsonResp;
+	}
 });

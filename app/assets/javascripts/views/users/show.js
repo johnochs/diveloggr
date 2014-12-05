099c8cd4e9dfe64eb0c3b5ba7a1868b258557757
John Ochs <johnochs@me.com>
2014-12-05 01:30:47 -0800

    Adding #addUserEntry, #removeUserEntry and some listeners

--- app/assets/javascripts/views/users/show.js
+++ app/assets/javascripts/views/users/show.js
@@ -4,8 +4,25 @@ Diveloggr.Views.UserShow = Backbone.CompositeView.extend({
 	initialize: function () {
 		this.listenTo(this.model, "sync", this.render);
 		if (this.model.entries){
+			this.listenTo(this.model.entries(), "add", this.addUserEntry);
+			this.listenTo(this.model.entries(), "remove", this.removeUserEntry);
 			this.listenTo(this.model.entries(), "sync", this.render);
-			this.model.entries().fetch({async: false});
+			this.model.entries().fetch({ async: false });
+		}
+		
+	},
+	addUserEntry: function (entry) {
+		var userEntryView = new Diveloggr.Views.UserEntry({ model: entry });
+		this.addSubview("#user-entry-subview-insertion", userEntryView);
+	},
+	removeUserEntry: function (entry) {
+		var userEntryView = _.find(
+			this.subviews("#user-entry-subview-insertion"), function (subview) {
+				return subview.model === entry;
+			}
+		);
+		if (userEntryView != undefined) {
+			this.removeSubview("#user-entry-subview-insertion", userEntryView);
 		}
 	},
 	render: function () {

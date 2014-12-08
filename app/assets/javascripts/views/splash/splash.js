68ce673324dc5fc26381df77bdb07feb9347631b
John Ochs <johnochs@me.com>
2014-12-08 01:57:06 -0800

    Splash page formatting is still messed up...

--- app/assets/javascripts/views/splash/splash.js
+++ app/assets/javascripts/views/splash/splash.js
@@ -1,5 +1,6 @@
 Diveloggr.Views.SplashPage = Backbone.CompositeView.extend({
 	template: JST['splash/splash'],
+	className: "splash-page",
 	render: function () {
 		var splashView = this.template();
 		this.$el.html(splashView)

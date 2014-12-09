6bdffa9ee8378808d0f9233da70932b79591306a
John Ochs <johnochs@me.com>
2014-12-09 13:38:43 -0800

    Making pacific timezone default

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Diveloggr
   class Application < Rails::Application
     
     config.filepicker_rails.api_key = ENV["FILEPICKER_API_KEY"]
+    config.time_zone = "Pacific Time (US & Canada)"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

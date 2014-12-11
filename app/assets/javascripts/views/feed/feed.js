b469e6f4920f9eb625593c9b37412362aa8b919a
John Ochs <johnochs@me.com>
2014-12-10 20:31:05 -0800

    Changing zeros to 'n/a' if no dives are in current view

--- app/assets/javascripts/views/feed/feed.js
+++ app/assets/javascripts/views/feed/feed.js
@@ -57,7 +57,7 @@ Diveloggr.Views.FeedView = Backbone.CompositeView.extend({
 		if(points.length > 0) {
 			return [points.length, average, min, mindate, max, maxdate];		
 		} else {
-			return [0,0,0,0,0,0];
+			return [0,"N/A","N/A","N/A","N/A","N/A"];
 		}
 	},
 	addFeedEntryView: function (entry) {

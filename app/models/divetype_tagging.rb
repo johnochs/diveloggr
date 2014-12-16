156bd1fa80e604ddee197c31214d880e982b8002
John Ochs <johnochs@me.com>
2014-12-15 22:00:29 -0800

    Adding validation on entry, not entry_id since entry may be new

--- app/models/divetype_tagging.rb
+++ app/models/divetype_tagging.rb
@@ -2,6 +2,6 @@ class DivetypeTagging < ActiveRecord::Base
   belongs_to :divetype
   belongs_to :entry
   
-  validates :entry_id, :divetype_id, presence: true
+  validates :entry, :divetype_id, presence: true
   validates :divetype_id, uniqueness: { scope: :entry_id }
 end

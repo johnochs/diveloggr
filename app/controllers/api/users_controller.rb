0d9d8fc580844655aac1c1cf6104c149046ba21e
John Ochs <johnochs@me.com>
2014-12-19 23:28:55 -0800

    Adding private methods for checking and validating password, also converting guest user to full user

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -20,23 +20,42 @@ class Api::UsersController < ApplicationController
   end
   
   def update
+    puts "O HAI! : "  
+    puts params
+    
     if params[:id].to_i != current_user.id
       render json: ["You don't have permission to do this."], status: :forbidden
-    else
-      @user = User.find(params[:id])
-    
-      if @user.update(user_params)
-        render json: @user, status: :ok
-      else
-        render json: @user.errors.full_messages, status: :unprocessable_entity
-      end
+      return
     end
+    
+    @user = User.find(params[:id])
+    if @user.update(user_params) && check_password
+      if params[:password] != ""
+        @user.password = params[:password]
+        @user.save!
+      end
+      full_account_conversion(@user)
+      render json: @user, status: :ok
+    else
+      render json: ["Password is not valid."], status: :unprocessable_entity
+    end
+    
   end
   
   private
   
   def user_params
-    params.require(:user).permit(:location, :age, :exp, :numdives, :fname, :lname)
+    params.require(:user).permit(:location, :age, :exp, :numdives, :fname, :lname, :password, :email, :guest)
+  end
+  
+  def check_password
+    params[:password] == params[:password_check]
+  end
+  
+  def full_account_conversion(user)
+    if (!user.pwdigest.nil? && !user.email.nil?)
+      user.update(guest: false)
+    end
   end
   
 end
